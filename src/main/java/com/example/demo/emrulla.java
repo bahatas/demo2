24969ebcaf8a8bf6ca35343f40eddde76ba70790
emrulla muhter <emrullauigur@gmai.com>
2023-07-06 23:30:25 +0300

    create new class

---
+++ src/main/java/com/example/demo/emrulla.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class emrulla {
+    public static void main(String[] args) {
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("emrulla");
+        }
+    }
+}
